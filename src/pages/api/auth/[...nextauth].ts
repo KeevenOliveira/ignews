5d4b817a8a9f3118d1b8e0967575f3b0526ddcf8
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2022-01-04 20:46:01 -0300

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 // import log from "loggin"
-console.log("environment variables: ")
+console.log("environment variables:")
 console.log(process.env.GITHUB_CLIENT_ID);
 console.log(process.env.GITHUB_CLIENT_SECRET);
 
