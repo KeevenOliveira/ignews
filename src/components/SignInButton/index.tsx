7954709e8dcc9b322010e47484ddaf52f5938698
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2022-01-11 21:42:40 -0300

    using FaunaDB to save user and Stripe button to subscribe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -6,34 +6,23 @@ import { signIn, useSession, signOut } from 'next-auth/react';
 export function SignInButton() {
 
     const { data } = useSession();
-    console.log("data");
-    console.log(data);
-    console.log("T29d91s5PC+GlG4MhStgwmghgkVRQq2+rsQAb0qkkko");
-    console.log("environment variables:")
-    console.log(process.env.GITHUB_CLIENT_ID);
-    console.log(process.env.GITHUB_CLIENT_SECRET);
-    console.log("SECRET: ");
-    console.log(process.env.SECRET);
-
-    console.log(process.env.NEXTAUTH_URL);
-    console.log(process.env.VERCEL_URL);
     return data ? (
-        <button 
+        <button
             type="button"
             className={styles.signInButton}
-            onClick={()=>signOut()}
-            >
-            <FaGithub color="#04D361"/>
+            onClick={() => signOut()}
+        >
+            <FaGithub color="#04D361" />
             {data.user.name}
-            <FiX color="#737380" className={styles.closeIcon}/>
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
-        <button 
+        <button
             type="button"
             className={styles.signInButton}
-            onClick={()=>signIn('github')}
-            >
-            <FaGithub color="#EBA417"/>
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color="#EBA417" />
             Sign in with GitHub
         </button>
     );
